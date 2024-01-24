c2559e4a82404b50b3b86633e7c74793d7fbea47
Paulo Telles <ptelles020@gmail.com>
2024-01-23 22:17:47 -0300

    adicionamento de entidade

---
+++ Models/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Crud_Angular_DotNet_Back.Models;
+
+[Table("Funcionarios")]
+public class Funcionario
+{
+    [Key]
+    public int FuncionarioId { get; set; }
+
+    [Required, StringLength(80)]
+    public string? NomeFuncionario { get; set; }
+
+    [Required, StringLength(80)]
+    public string? Cargo { get; set; }
+
+    [Required, Range(1, 100)]
+    public int IdadeFuncionario { get; set; }
+
+    [Required]
+    public bool IsActive { get; set; }
+
+    [Required]
+    public bool IsInative { get; set; }
+}
