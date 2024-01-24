31264a0ee71a143f336187b14c187ee15e581399
Paulo Telles <ptelles020@gmail.com>
2024-01-23 22:26:48 -0300

    adição da injeção de dependência

---
+++ Controllers/FuncionariosController.cs
@@ -0,0 +1,16 @@
+﻿using Crud_Angular_DotNet_Back.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud_Angular_DotNet_Back.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FuncionariosController : ControllerBase
+{
+    [HttpGet]
+    public Task<ActionResult<IEnumerable<Funcionario>>> GetFuncionarios()
+    {
+
+    }
+}
