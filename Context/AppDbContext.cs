77cf87feb56ab864fa178e1abaa4f7b4bc785300
Paulo Telles <ptelles020@gmail.com>
2024-01-23 22:20:18 -0300

    contexto inserido

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Crud_Angular_DotNet_Back.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_Angular_DotNet_Back.Context;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+
+    public DbSet<Funcionario> Funcionarios { get; set; }
+}
