dd24a17e03482c06edea8a927e9eae56d1fd0f06
Paulo Telles <ptelles020@gmail.com>
2024-01-23 22:52:16 -0300

    Adicionamento do Migrations

---
+++ Migrations/20240124015053_MigracaoInicial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Crud_Angular_DotNet_Back.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    FuncionarioId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    NomeFuncionario = table.Column<string>(type: "NVARCHAR2(80)", maxLength: 80, nullable: false),
+                    Cargo = table.Column<string>(type: "NVARCHAR2(80)", maxLength: 80, nullable: false),
+                    IdadeFuncionario = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    IsActive = table.Column<bool>(type: "NUMBER(1)", nullable: false),
+                    IsInative = table.Column<bool>(type: "NUMBER(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionarios", x => x.FuncionarioId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Funcionarios");
+        }
+    }
+}
